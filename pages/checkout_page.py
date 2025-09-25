0f064db9f36b34ab8b15ed6b0122cd2f40c0976d
Muskaan Sahu <muskaans1341@gmail.com>
2025-09-25 19:21:32 +0530

    tests

--- pages/checkout_page.py
+++ pages/checkout_page.py
@@ -1,7 +1,9 @@
 
+import selenium
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from selenium.common.exceptions import TimeoutException
 
 class CheckoutPage:
     def __init__(self, driver):
@@ -10,22 +12,26 @@ class CheckoutPage:
         self.last_name = (By.ID, "last-name")
         self.postal_code = (By.ID, "postal-code")
         self.continue_btn = (By.ID, "continue")
-        self.finish_btn = (By.ID, "finish")
+        self.finish_btn = (By.XPATH, "//*[@id='finish']")
         self.success_msg = (By.CLASS_NAME, "complete-header")
         self.error_message = (By.CSS_SELECTOR, "[data-test='error']")
 
 
-    def fill_information(self, fname, lname, postal):
-        WebDriverWait(self.driver, 20).until(
-        EC.visibility_of_element_located(self.first_name)
-        ).send_keys(fname)
-        self.driver.find_element(*self.last_name).send_keys(lname)
-        self.driver.find_element(*self.postal_code).send_keys(postal)
-        self.driver.find_element(*self.continue_btn).click()
-        # Wait for the Finish button to be present after clicking Continue
-        WebDriverWait(self.driver, 20).until(
-            EC.presence_of_element_located(self.finish_btn)
-        )
+    def fill_information(self, first_name, last_name, postal_code):
+        try:
+            WebDriverWait(self.driver, 15).until(EC.presence_of_element_located(self.first_name)).clear()
+            self.driver.find_element(*self.first_name).send_keys(first_name)
+        
+            WebDriverWait(self.driver, 15).until(EC.presence_of_element_located(self.last_name)).clear()
+            self.driver.find_element(*self.last_name).send_keys(last_name)
+            
+            WebDriverWait(self.driver, 15).until(EC.presence_of_element_located(self.postal_code)).clear()
+            self.driver.find_element(*self.postal_code).send_keys(postal_code)
+        
+            WebDriverWait(self.driver, 10).until(EC.element_to_be_clickable(self.continue_btn)).click()
+        except TimeoutException: 
+            self.driver.save_screenshot("checkout_fill_info_timeout.png")
+            # raise AssertionError("Checkout form did not appear or was not interactable.")
 
     def finish_order(self):
         # Wait for Finish button before clicking
--- tests/test_checkout.py
+++ tests/test_checkout.py
@@ -1,6 +1,4 @@
 import selenium
-import time
-
 from selenium.webdriver.chrome.webdriver import WebDriver
 from pages.login_page import LoginPage
 from utils import config
@@ -35,9 +33,9 @@ def test_checkout_valid(setup: tuple[WebDriver, LoginPage]):
     success_text = checkout.get_success_message()
     assert "THANK YOU FOR YOUR ORDER" in success_text.upper()
 
-# ---------- Scenario 2: checkout with invalid information----------
+# ---------- Scenario 2:  Partially empty fields ----------
 
-def test_checkout_invalid(setup: tuple[WebDriver, LoginPage]):
+def test_checkout_partial_empty_filed(setup: tuple[WebDriver, LoginPage]):
     driver, login =setup
     login.login (config.valid_username, config.valid_password)
 
@@ -49,10 +47,45 @@ def test_checkout_invalid(setup: tuple[WebDriver, LoginPage]):
     cart.proceed_to_checkout()
 
     checkout = CheckoutPage(driver)
-    checkout.fill_information("M","Sahu","856")
-    checkout.finish_order()
-    error_text = checkout.get_error_message()
+    checkout.fill_information("","QA","856")
+    error_text = checkout.get_error_message()                                                                                                       
+
     assert "Error: First Name is required" in error_text
 
-# ---------- Scenario 3: Partially empty fields ----------
+# ---------- Scenario 3: checkout with invalid information----------
+def test_checkout_invalid_info(setup: tuple[WebDriver, LoginPage]):
+    driver, login =setup
+    login.login (config.valid_username, config.valid_password)
+
+    inventory = InventoryPage(driver)
+    inventory.add_to_cart()
+    inventory.go_to_cart()
+    
+    cart = CartPage(driver)
+    cart.proceed_to_checkout()
+
+    checkout = CheckoutPage(driver)
+    checkout.fill_information("123","QA","856")
+    checkout.finish_order()
+
+    success_text = checkout.get_success_message()
+    assert "THANK YOU FOR YOUR ORDER" in success_text.upper()
+
+
 # ---------- Scenario 4: All empty fields ----------
+def test_checkout_all_empty_filed(setup: tuple[WebDriver, LoginPage]):
+    driver, login =setup
+    login.login (config.valid_username, config.valid_password)
+
+    inventory = InventoryPage(driver)
+    inventory.add_to_cart()
+    inventory.go_to_cart()
+    
+    cart = CartPage(driver)
+    cart.proceed_to_checkout()
+
+    checkout = CheckoutPage(driver)
+    checkout.fill_information("","","")
+    error_text = checkout.get_error_message()                                                                                                       
+
+    assert "Error: First Name is required" in error_text
